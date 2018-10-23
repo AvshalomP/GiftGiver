f83cc62212ca714d142c018b3c755aa10cbaadaf
avshalomp <avshi1987@gmail.com>
2018-10-23 13:47:30 +1300

    add button to add new gift to state

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { gifts: [] }
+    }
+
+    handleAddGift = () => {
+        const { gifts } = this.state;
+        const ids = gifts.map( gift => gift.id);
+        const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+
+        gifts.push({ id: maxId+1});
+        this.setState({ gifts });
+    };
+
     render(){
         return(
             <div>
                 <h2>Gift Giver</h2>
+                <button className="btn-add" onClick={this.handleAddGift}>Add Gift</button>
             </div>
         )
     }
--- src/test/App.test.js
+++ src/test/App.test.js
@@ -2,10 +2,23 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../components/App';
 
-const app = shallow(<App />);
 
 describe('App', () => {
+    const app = shallow(<App />);
+
     it('renders correctly', () => {
         expect(app).toMatchSnapshot();
-    })
+    });
+
+    it('initializes the `state` with an empty list of gifts', () => {
+       expect(app.state().gifts).toEqual([]);
+    });
+
+    describe('when clicking the `add gift` button', () => {
+        it('adds a new gift to `state`', () => {
+            app.find('.btn-add').simulate('click');
+
+            expect(app.state().gifts).toEqual([{ id: 1 }]);
+        });
+    });
 });
