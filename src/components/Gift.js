b995ee0d8dc820b81437d8ab98da1d83ff9ba321
avshalomp <avshi1987@gmail.com>
2018-10-26 12:57:13 +1300

    add Gift form

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -4,11 +4,27 @@ class Gift extends Component {
     constructor(props){
         super(props);
 
-        this.state = { person: "", present: "" }
+        this.state = { person: "", present: "" };
     }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
     render(){
+        console.log(this.state);
         return (
-            <div>GIFT</div>
+            <form>
+                <label htmlFor="person">Person:</label>
+                <input type="text" className="input-person" name="person"
+                       onChange={this.handleChange}/>
+                <label htmlFor="present">Present:</label>
+                <input type="text" className="input-present" name="present"
+                       onChange={this.handleChange}
+                />
+            </form>
         )
     }
 }
--- src/test/Gift.test.js
+++ src/test/Gift.test.js
@@ -4,14 +4,38 @@ import Gift from '../components/Gift';
 
 
 describe('Gift', () => {
-    const gift = shallow(<Gift />);
+   const gift = shallow(<Gift />);
 
-    it('should render properly', () => {
-        expect(gift).toMatchSnapshot();
-    });
+   it('should render properly', () => {
+      expect(gift).toMatchSnapshot();
+   });
 
-    it('initializes a person and present in `state`', () => {
-        expect(gift.state()).toEqual({ person: "", present: "" });
-    });
+   it('initializes a person and present in `state`', () => {
+      expect(gift.state()).toEqual({ person: "", present: "" });
+   });
+
+   describe('when typing into the person input', () => {
+      const person = 'Uncle';
+
+      beforeEach(() => {
+          gift.find('.input-person').simulate('change', { target: { name: "person", value: person } });
+      });
+
+      it('updates the person in `state`', () => {
+          expect(gift.state().person).toEqual(person);
+      })
+   });
+
+   describe('when typing into the present input', () => {
+      const present = 'Wine';
+
+      beforeEach(() => {
+         gift.find('.input-present').simulate('change', { target: { name: "present", value: present} });
+      });
+
+      it('updates the present in `state`', () => {
+          expect(gift.state().present).toEqual(present);
+      });
+   });
 });
 
